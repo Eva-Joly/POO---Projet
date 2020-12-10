ad81b9cde5471a9ac4747af8fe0a9a21671f06c3
Houss83 <74711795+Houss83@users.noreply.github.com>
2020-12-10 11:45:03 +0100

    Update expression.h

--- Commun/NTI_Projet/expression.h
+++ Commun/NTI_Projet/expression.h
@@ -12,5 +12,8 @@ public:
     virtual float calculer()=0;
     virtual void afficherClassique(ostream &reference)=0;
     virtual void afficherPolonaise(ostream &reference)=0;
+    static Expression Creer();
+    
+    //Attributs :
 };
 #endif // EXPRESSION_H
